fd4499958e89c49fad965bd10b2f9f0e8cd7cf57
Daniel Pombo <pombo.daniel@gmail.com>
2019-05-13 11:54:40 -0300

    Possible moves of a piece

--- src/chess/chessException.java
+++ src/chess/chessException.java
@@ -2,10 +2,10 @@
 
 import boardgame.BoardException;
 
-public class chessException extends BoardException{
+public class ChessException extends BoardException{
 	private static final long serialVersionUID = 1L;
 
-	public chessException(String msg) {
+	public ChessException(String msg) {
 		super(msg);
 	}
 }
